ac7deb9fad4cb1c7f611fcc3d9547adf629a67c9
Kailas Moon <kailasmoon@gmail.com>
2020-05-02 19:07:50 -0400

    Blockstack functionality

---
+++ dist/index.js
@@ -0,0 +1,57 @@
+const appConfig = new blockstack.AppConfig();
+const userSession = new blockstack.UserSession({ appConfig: appConfig });
+var cards = [];
+
+function getCards() {
+  const options = { decrypt: true };
+  userSession.getFile("cards", options).then(function(content) {
+    if (content) {
+      cards = JSON.parse(content)
+    }
+  })
+}
+
+function saveCard() {
+  const options = { encrypt: true };
+  userSession.putFile("cards", JSON.stringify(cards), options);
+}
+
+function newCard(name, number) {
+  cards.push({
+    name: name,
+    number: number
+  });
+  saveCards()
+}
+
+function deleteCard(card) {
+  for (i=0; i<cards.length; i++) {
+    if (cards[i].name === card.name && cards[i].number === card.number) {
+      cards.splice(i, 1);
+      return;
+    }
+  }
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+    document.getElementById('signin-button').addEventListener('click', function (event) {
+      event.preventDefault();
+      userSession.redirectToSignIn();
+    })
+    document.getElementById('signout-button').addEventListener('click', function (event) {
+      event.preventDefault();
+      userSession.signUserOut(window.location.href);
+    })
+
+    if (userSession.isUserSignedIn()) {
+      var profile = userSession.loadUserData().profile
+      document.getElementById('section-1').style.display = "none";
+      document.getElementById('section-2').style.display = "block";
+      document.getElementById('username').innerHTML = userSession.loadUserData().profile.name;
+      getCards();
+    } else if (userSession.isSignInPending()) {
+      userSession.handlePendingSignIn().then(function (userData) {
+        window.location = window.location.origin;
+      })
+    }
+  })
