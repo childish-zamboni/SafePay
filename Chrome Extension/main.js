f809ca418e2813811594bc6e88eaf96000db331a
Kailas Moon <kailasmoon@gmail.com>
2020-05-03 11:09:54 -0400

    Finishing up

--- Chrome Extension/main.js
+++ Chrome Extension/main.js
@@ -26,8 +26,9 @@ document.addEventListener('DOMContentLoaded', function() {
 
 function displayCards(myCards) {
   var myCards = localStorage.getItem('cards_str');
+  myCards = JSON.parse(myCards);
   var list = document.getElementById('cards')
   for (var i=0; i<myCards.length; i++) {
-    list.innerHTML += '<li><div class="collapsible-header"><i class="material-icons">payment</i>' + myCards[0].label + '</div><div class="collapsible-body"><span class="white-text">'+ myCards[0].number +'</span><div class = "row"><span class="white-text">'+ myCards[0].date +'</span></div></div></li>'
+    list.innerHTML += '<li><div class="collapsible-header"><i class="material-icons">payment</i>' + myCards[i].label + '</div><div class="collapsible-body"><span class="white-text">Number : ' + myCards[i].number + '</span><div><span class="white-text">Cardholder : ' + myCards[i].name + '</span></div><div><span class="white-text">Expiry Date : ' + myCards[i].date + '</span></div></div></li>'
   }
 }
