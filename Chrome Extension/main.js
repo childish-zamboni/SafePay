08916de822f3f6e9c6a35c66c4961faf87d40325
VictorChen <victor.chen1447@gmail.com>
2020-05-02 20:50:01 -0400

    bug fix

--- Chrome Extension/main.js
+++ Chrome Extension/main.js
@@ -1,6 +1,1 @@
 'use strict';
-
-document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.modal');
-    var instances = M.Modal.init(elems, options);
-  });
